522459b764008917efcb5d81dff1e541d5539800
Ilya Savva <ilya.savva8@gmail.com>
2021-09-11 20:43:11 +0300

    fix post request

--- product-service/src/functions/postProduct/handler.ts
+++ product-service/src/functions/postProduct/handler.ts
@@ -1,21 +1,33 @@
 import 'source-map-support/register';
 
+import type { APIGatewayProxyEvent } from 'aws-lambda';
 import { formatResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 import { dbConnection } from '@libs/dbConnection';
 
-const postProduct = async () => {
+const postProduct = async (event: APIGatewayProxyEvent) => {
+  console.log(event.body);
+  const { title, description, price, count } = event.body;
   const connection = await dbConnection();
   let data;
 
   try {
-    console.log('post')
+    data = await connection.query(`
+      WITH ins AS (
+        INSERT INTO products (title, description, price)
+        VALUES ('${title}', '${description}', '${price}')
+        RETURNING id
+        )
+      INSERT INTO stocks (product_id, count)
+      SELECT id, '${count}' FROM ins
+      RETURNING product_id;
+    `);
   } catch(err) {
     console.error('Error on db query', err);
   } finally {
     connection.end();
   }
-  return formatResponse({ data }, 200);
+  return formatResponse({ product_id: data.rows[0].product_id }, 200);
 }
 
 export const main = middyfy(postProduct);
